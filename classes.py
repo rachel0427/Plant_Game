52a40aec8536df461dfd6c846fb81d87009126cc
Rachel <ruoqi.yang@vanderbilt.edu>
2021-06-27 00:26:47 +0800

    working vodka bottle count

--- classes.py
+++ classes.py
@@ -6,29 +6,43 @@ from pygame import mixer
 # This file documents all classes of objects used in the game
 # 整个游戏一共5瓶vodka，救急
 
+# TODO: mood bubble should only be displayed
+#  when plant has already been placed on shelf
+# FIXME: if plant already on shelf, it can't be moved back to original position
+
 # round_num keeps track of the current round number
 global round_num
 global plant_size
 global shelf_rect_list
 
-
 # Some Variables
 InitialMoney = 50
 plant_size = 64
 # rectangle values: left, top, width, height
-shelf_rect_list = [pygame.Rect(270,178,120,100), pygame.Rect(390,178,120,100), pygame.Rect(510,178,120,100),
-                   pygame.Rect(270,315,120,100), pygame.Rect(390,315,120,100), pygame.Rect(510,315,120,100),
-                   pygame.Rect(270,453,120,100), pygame.Rect(390,453,120,100), pygame.Rect(510,453,120,100)]
+shelf_rect_list = [pygame.Rect(270, 178, 120, 100), pygame.Rect(390, 178, 120, 100), pygame.Rect(510, 178, 120, 100),
+                   pygame.Rect(270, 315, 120, 100), pygame.Rect(390, 315, 120, 100), pygame.Rect(510, 315, 120, 100),
+                   pygame.Rect(270, 453, 120, 100), pygame.Rect(390, 453, 120, 100), pygame.Rect(510, 453, 120, 100)]
 
+# shelf_if_occupied keeps track of if a grid on shelf is occupied
+shelf_is_occupied = [False, False, False, False, False, False, False, False, False]
 
+# A Lot of Images
 GoodPlantImage = 'Images/goodPlant.png'
 GreatPlantImage = 'Images/greatPlant.png'
 AwesomePlantImage = 'Images/awesomePlant.png'
 LeekImage = 'Images/leek.png'
 TrashCanImage = 'Images/trashCan.png'
+NextButtonImage = 'Images/next.png'
+MoodBubbleImage = 'Images/moodBubble.png'
+BigGrandPaImage = 'Images/bigGrandpa - Copy.jpg'
+HappyImage = 'Images/happy.png'
+SadImage = 'Images/sad.png'
+AngryImage = 'Images/angry.png'
+VodkaImage = 'Images/vodka.png'
 
 ExplodeState = 3  # plants explode when anger state reaches 3
 
+# A lot of Variables
 SellGood = 50
 SellGreat = 60
 SellAwesome = 100
@@ -48,6 +62,11 @@ GreatProduceCycle = 1
 AwesomeProduceCycle = 1
 LeekProduceCycle = 1
 
+# font
+font = pygame.font.Font('freesansbold.ttf', 32)
+
+
+# Here Comes the Classes
 
 class Game:
     # Use this class to keep track of and relate all elements?
@@ -57,7 +76,7 @@ class Game:
 
 # Button Proceed
 class NextButton:
-    img = pygame.image.load('Images/next.png')
+    img = pygame.image.load(NextButtonImage)
     pos_x = 10
     pos_y = 570
 
@@ -74,21 +93,124 @@ class NextButton:
             return True
         return False
 
+
 # Anger Bar
-class AngerBar:
-    pass
+class MoodBubble:
+    happy = pygame.image.load(HappyImage)
+    sad = pygame.image.load(SadImage)
+    angry = pygame.image.load(AngryImage)
+    pos_x = 0
+    pos_y = 0
+    plus_x = 20
+    plus_y = -30
+
+    def show_bubble(self, window, plant_pos_x, plant_pos_y, anger_level):
+        """
+        This function displays bubble based on plant position
+        :param anger_level:
+        :param window: game window
+        :param plant_pos_x: x position of plant whose mode is to be displayed
+        :param plant_pos_y: y position of plant whose mode is to be displayed
+        """
+        # This is just shifting the bubble based on plant position
+        self.pos_x = plant_pos_x + self.plus_x
+        self.pos_y = plant_pos_y + self.plus_y
+        # Making sure that bubble position is initialized
+        if self.pos_x != 0 and self.pos_y != 0:
+            if anger_level == 0:
+                window.blit(self.happy, (self.pos_x, self.pos_y))
+            elif anger_level == 1:
+                window.blit(self.sad, (self.pos_x, self.pos_y))
+            elif anger_level == 2:
+                window.blit(self.angry, (self.pos_x, self.pos_y))
+
+
+class VodkaCount:
+
+    pos_x = 760
+    pos_y = 220
+
+    def __init__(self, count=5):
+        self.count = count
+
+    def show_num(self, screen):
+        count = font.render(": " + str(self.count), True, (0, 0, 0))
+        screen.blit(count, (self.pos_x, self.pos_y))
 
 
 # Vodka Bottle
-class VodkaBottleIcon:
-    pass
+class VodkaBottle:
+
+    img = pygame.image.load(VodkaImage)
+    pos_x = 700
+    pos_y = 200
+
+    def __init__(self, num=5):
+        self.num = num
+
+    def show_vodka_image(self, window):
+        """
+        Show vodka bottle
+        :param window: game window
+        """
+        window.blit(self.img, (self.pos_x, self.pos_y))
+
+    def check_vodka_collision(self, mouse_x, mouse_y):
+        """
+        Check if mouse collide with vodka bottle image
+        :param mouse_x: x coord of mouse position
+        :param mouse_y: y coord of mouse position
+        :return: boolean value of whether collision occured
+        """
+        rectangle = self.img.get_rect()
+        rectangle.topleft = (self.pos_x, self.pos_y)
+        if rectangle.collidepoint(mouse_x, mouse_y):
+            return True
+        return False
+
+    def drag_vodka_bottle(self, mouse_x, mouse_y, vodka_count_obj):
+        if vodka_count_obj.count > 0:
+            self.pos_x = mouse_x - 32
+            self.pos_y = mouse_y - 32
+
+    def check_plant_vodka_collision(self, plant_list, vodka_count_obj):
+        """
+        This function checks if vodka bottle collide with plant
+        :param plant_list:
+        :return:
+        """
+        if vodka_count_obj.count > 0:
+            closest_rect_ind = 0
+            min_dist = math.sqrt(
+                math.pow((plant_list[0].pos_x - self.pos_x), 2) +
+                math.pow((plant_list[0].pos_y - self.pos_y), 2))
+            for i in range(len(plant_list)):
+                cur_dist = math.sqrt(math.pow(plant_list[i].pos_x - self.pos_x, 2) +
+                                     math.pow(plant_list[i].pos_y - self.pos_y, 2))
+                if cur_dist < min_dist:
+                    closest_rect_ind = i
+                    min_dist = cur_dist
+
+            if min_dist < 80:
+                plant_list[closest_rect_ind].decrease_anger()
+                self.pos_x = 700
+                self.pos_y = 200
+                vodka_count_obj.count -= 1
 
 
 # Money Keeper
 class Bank:
 
+    pos_x = 700  # position of money count
+    pos_y = 150
+
     def __init__(self, money=50):
         self.money = money  # player's possession is initialized to 50
+
+    def show_money(self, screen):
+        # rander: (text, TURE, color_in_RGB)
+        score = font.render("Money: " + str(self.money), True, (0, 0, 0))
+        screen.blit(score, (self.pos_x, self.pos_y))
 
     # Sell Plant functions will be called when player sells their plant
     def sell_good_plant(self):
@@ -124,6 +246,7 @@ class Bank:
     def awesome_produce(self):
         self.money += AwesomeProduce
 
+
 # Trash can object: drag plants to trash icon to delete object
 class TrashCan:
     img = pygame.image.load(TrashCanImage)
@@ -134,65 +257,182 @@ class TrashCan:
     def show_trash_can(self, window):
         window.blit(self.img, (self.pos_x, self.pos_y))
 
+
+# Big Grandpa
+class BigGrandpa:
+    img = pygame.image.load(BigGrandPaImage)
+    pos_x = 50
+    pos_y = 500
+    font_x = 50
+    font_y = 460
+    font = pygame.font.Font('Fonts/LazySunday-Regular.ttf', 48)
+
+    # show_big_grandpa_font shows big grandpa with his title on screen
+    def show_big_grandpa_font(self, window):
+        big_grandpa_title = self.font.render("Big Grandpa", True, (0, 0, 0))
+        window.blit(self.img, (self.pos_x, self.pos_y))
+        window.blit(big_grandpa_title, (self.font_x, self.font_y))
+
+
 # Plants: 4 species
 # Class for Good Plants
 class GoodPlant:
-
     # Good Plant Properties
     sell_price = SellGood
     production = GoodProduce
     production_cycle = GoodProduceCycle
     img = pygame.image.load(GoodPlantImage)
-    anger_state = 0
+    anger_level = 0
+    # TODO: added this variable
+    # this variable keeps track of whether the plant has been placed on the shelf
+    # if plant has been placed, then it starts to have a mood bubble
+    # otherwise, the plant does not have a mood bubble
+    on_shelf = False
+    shelf_pos_cur = -1  # Initialized to -1
+    shelf_pos_prev = -1
 
     # Constructor
     def __init__(self, pos_x, pos_y, shelf_pos):
         self.pos_x = pos_x
         self.pos_y = pos_y
-        self.shelf_pos = 0  # Initialized to top shelf
+        self.init_x = pos_x  # keeps track of plant's initial postion when created
+        self.init_y = pos_y
 
-    # show_good_plant shows the good plant object on screen
+    def decrease_anger(self):
+        self.anger_level -= 1
+
+    def increase_anger(self):
+        self.anger_level += 1
+
     def show_good_plant(self, window):
+        """
+        # show_good_plant shows the good plant object on screen
+        :param window: the game window
+        """
         window.blit(self.img, (self.pos_x, self.pos_y))
 
-    # check_good_collision checks for collision between mouse and good plant
     def check_good_collision(self, mouseX, mouseY):
-        # This is how you check for collision between mouse pos and objects
+        """
+         Check_good_collision checks for collision between mouse and good plant
+        :param mouseX: x coord of mouse position
+        :param mouseY: y coord of mouse position
+        :return: boolean value of whether collision occured
+        """
+        # get a rectangle the size of img
         rectangle = self.img.get_rect()
+        # place the rectangle so that its top left corner is placed at (pos_x, pos_y)
         rectangle.topleft = (self.pos_x, self.pos_y)
+        # check for collision with the collidepoint() function
         if rectangle.collidepoint(mouseX, mouseY):
             return True
         return False
 
-    # drag_good_plant makes good plant follow mouse position
     def drag_good_plant(self, mouseX, mouseY):
+        """
+        Drag_good_plant makes good plant follow mouse position
+        :param mouseX: x coord of mouse position
+        :param mouseY: y coord of mouse position
+        """
         self.pos_x = mouseX - plant_size / 2
         self.pos_y = mouseY - plant_size / 2
 
-    # check_trash_plant_collision checks for collision with trash can
     def check_trash_good_collision(self, trash_obj):
+        """
+        Check_trash_plant_collision checks for collision with trash can
+        :param trash_obj: trash_can object
+        :return: boolean value of whether trash can and plant object collided
+        """
         # This is how you check for collision between trash can and plant objects
         distance = math.sqrt(
             math.pow((trash_obj.pos_x - self.pos_x), 2) + math.pow((trash_obj.pos_y - self.pos_y), 2))
         if distance < 57:
             return True
         return False
-    # correct_good_pos corrects good plant's position as player drags it on the shelf
+
+    def check_mouse_on_plant(self, mouse_x, mouse_y):
+        """
+         This function checks if mouse is on this plant
+        :param mouse_x: x coord of mouse position
+        :param mouse_y: y coord of mouse position
+        :return: whether mouse is placed on plant image
+        """
+        # same as above
+        rectangle = self.img.get_rect()
+        rectangle.topleft = (self.pos_x, self.pos_y)
+        if rectangle.collidepoint(mouse_x, mouse_y):
+            return True
+        return False
+
     def correct_good_pos(self):
-        pass
-
-    def increase_anger(self):
-        """Called when plant is moved up the shelf
         """
-        self.anger_state += 1
-
-    def decrease_anger(self):
-        """Called when plant is moved down the shelf
+        Correct_good_pos corrects good plant's position as player drags it on the shelf
         """
-        self.anger_state -= 1
+        closest_rect_ind = 0  # closest_rect_ind keeps track of the index of the closest rectangle
+        min_dis = math.sqrt(
+            math.pow((shelf_rect_list[0].topleft[0] - self.pos_x), 2) +
+            math.pow((shelf_rect_list[0].topleft[1] - self.pos_y), 2))
+
+        # find closest shelf position
+        for i in range(1, len(shelf_rect_list)):
+            cur_dist = math.sqrt(
+                math.pow((shelf_rect_list[i].topleft[0] - self.pos_x), 2) +
+                math.pow((shelf_rect_list[i].topleft[1] - self.pos_y), 2))
+            if cur_dist < min_dis:
+                closest_rect_ind = i
+                min_dis = cur_dist
+
+        # after for loop, min dist and index are found
+        # check if plant was even dragged to the shelf; if not, back to initial position
+        if min_dis < 80 and (not shelf_is_occupied[closest_rect_ind]):
+            self.change_position(shelf_rect_list[closest_rect_ind].topleft[0] + 30,
+                                 shelf_rect_list[closest_rect_ind].topleft[1] + 20)
+            shelf_is_occupied[closest_rect_ind] = True
+            # update plant position
+            self.shelf_pos_cur = closest_rect_ind
+            self.on_shelf = True
+        else:
+            self.change_position(self.init_x, self.init_y)
+
+    # clear_shelf resets shelf grid to empty when plant dragged away
+    # called when mouse button down
+    def clear_shelf(self):
+        closest_rect_ind = 0  # closest_rect_ind keeps track of the index of the closest rectangle
+        min_dis = math.sqrt(
+            math.pow((shelf_rect_list[0].topleft[0] - self.pos_x), 2) +
+            math.pow((shelf_rect_list[0].topleft[1] - self.pos_y), 2))
+
+        for i in range(1, len(shelf_rect_list)):
+            cur_dist = math.sqrt(
+                math.pow((shelf_rect_list[i].topleft[0] - self.pos_x), 2) +
+                math.pow((shelf_rect_list[i].topleft[1] - self.pos_y), 2))
+            if cur_dist < min_dis:
+                closest_rect_ind = i
+                min_dis = cur_dist
+        # after for loop, min dist and index are found
+        # check if plant was on shelf initially
+        if min_dis < 80:
+            shelf_is_occupied[closest_rect_ind] = False
+
+    def initialize_anger(self):
+        if 0 <= self.shelf_pos_cur <= 2:
+            self.anger_level = 0
+        elif 3 <= self.shelf_pos_cur <= 5:
+            self.anger_level = 1
+        elif 6 <= self.shelf_pos_cur <= 8:
+            self.anger_level = 2
+
+    """
+    def set_anger_shelf(self):
+        # if plant was moved down
+        if self.shelf_pos_cur < self.shelf_pos_prev:
+            self.anger_level += 1
+        elif self.shelf_pos_cur > self.shelf_pos_prev:  # if plant was moved up
+            self.anger_level -= 1
+    """
 
     def explode(self):
-        """Called when anger level of plant >= 3, destroys plant
+        """
+        Called when anger level of plant >= 3, destroys plant
         note: 如果plant anger level到达了3，则在round里call explode function来destroy plant
         """
         if self.anger_state >= ExplodeState:
@@ -200,7 +440,8 @@ class GoodPlant:
             pass
 
     def change_position(self, end_pos_x, end_pos_y):
-        """Called in main.py's game while loop,
+        """
+        Called in main.py's game while loop,
         note: 当到达一定round, called in Game? or round? to create new good plants.
         """
         self.pos_x = end_pos_x
@@ -209,7 +450,6 @@ class GoodPlant:
 
 # Great Plant
 class GreatPlant:
-
     # Class Variables
     sell_price = SellGreat
     production = GreatProduce
@@ -251,7 +491,6 @@ class GreatPlant:
 
 
 class AwesomePlant:
-
     # Awesome Plant Properties
     sell_price = SellAwesome
     production = AwesomeProduce
@@ -260,7 +499,6 @@ class AwesomePlant:
 
     # Constructor
     def __init__(self, need_vodka_in, anger_state, produce_in, pos_x, pos_y, shelf_pos):
-
         self.need_vodka_in = need_vodka_in  # Initialized to 0; Assume need vodka first round when created
         self.anger_state = anger_state  # Initialized to 0; start out not angry
         self.produce_in = produce_in  # Initialized to 1; Assume first reproduction on next round
@@ -295,7 +533,6 @@ class AwesomePlant:
 
 
 class Leek:
-
     # Leek Plant Properties
     sell_price = SellLeek
     production = LeekProduce
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import pygame
+
+# Initialize pygame
+pygame.init()
+
 import random
 import math
 from pygame import mixer
@@ -13,15 +17,22 @@ global AllPlant_count
 global dragged_type
 global dragged_ind
 global stage  # stage keeps track of which page game is currently on
+global chat_box_width
+global chat_box_height
+global big_grandpa_text_str
+global big_grandpa_text
+global big_grandpa_text_num
+global delete
 
 screen_width = 900
 screen_height = 650
 shelf_size = 512
+chat_box_width = 728
+chat_box_height = 670
 dragging = False
 stage = 0  # in stage 0, player can vodka and drag plants on shelf
-
-# Initialize pygame
-pygame.init()
+font = pygame.font.Font('Fonts/LazySunday-Regular.ttf', 32)
+big_grandpa_text_num = 0
 
 # create window, width & height
 screen = pygame.display.set_mode((screen_width, screen_height))
@@ -30,6 +41,10 @@ center_of_screen = (screen_width / 2, screen_height / 2)
 # Background
 background = pygame.image.load('Images/shelf1.png')
 background_top_left = (center_of_screen[0] - shelf_size / 2, center_of_screen[1] - shelf_size / 2 + 40)
+
+# chat box background
+chat_box = pygame.image.load('Images/dialogue.jpg')
+chat_box_top_left = (center_of_screen[0] - chat_box_width / 2, center_of_screen[1] - chat_box_height / 2)
 
 # Title and Icon
 pygame.display.set_caption("Plants of Wrath")
@@ -65,11 +80,56 @@ def create_new_good_plant():
     AllPlant_count += 1
 
 
+def show_text_in_box(surface, font, x_start, x_end, y_start, text, colour):
+    x = x_start
+    y = y_start
+    words = text.split(' ')
+
+    for word in words:
+        word_t = font.render(word, True, colour)
+        if word_t.get_width() + x <= x_end:
+            surface.blit(word_t, (x, y))
+            x += word_t.get_width() + 2
+        else:
+            y += word_t.get_height() + 4
+            x = x_start
+            surface.blit(word_t, (x, y))
+            x += word_t.get_width() + 2
+
+
+# Create bank object
+bank = Bank()
+
 # Create trash can object
 trash_can = TrashCan()
 
 # Create next button object
 next_button = NextButton()
+
+# Create mood bubble object
+mood_bubble = MoodBubble()
+
+# Create vodka
+vodka = VodkaBottle()
+
+# vodka count
+vodka_count = VodkaCount()
+
+# Create big grandpa object
+big_grandpa = BigGrandpa()
+
+# array for big grandpa's text
+big_grandpa_text_str = ["Important Message1: Lorem ipsum dolor sit amet, consectetur "
+                        "adipiscing elit, sed do eiusmod tempor incididunt ut labore "
+                        "et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud "
+                        "exercitation ullamco laboris nisi ut aliquip ex ea commodo "
+                        "consequat.",
+                        "Important Message2: Duis aute irure dolor in reprehenderit "
+                        "in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "
+                        "Excepteur sint occaecat cupidatat non proident, sunt in culpa "
+                        "qui officia deserunt mollit anim id est laborum?"]
+big_grandpa_text = [font.render(big_grandpa_text_str[0], True, (0, 0, 0)),
+                    font.render(big_grandpa_text_str[1], True, (0, 0, 0))]
 
 # Create 3 good plants
 for i in range(3):
@@ -87,12 +147,33 @@ while running:
         # Background Image
         screen.blit(background, background_top_left)
 
+        # show colors for blocks
+        red = 0
+        green = 255
+        for i in range(len(shelf_rect_list)):
+            pygame.draw.rect(screen, (red, green, 0), shelf_rect_list[i])
+            red += 25
+            green -= 25
+
+        # Show money
+        bank.show_money(screen)
+        vodka_count.show_num(screen)
+
+        # Display mood Bubble if mouse is hovering over a plant
+        # that has already been placed on shelf
+        for plant in GoodPlantList:
+            plant.initialize_anger()
+            if plant.check_mouse_on_plant(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])\
+                    and plant.on_shelf:
+                mood_bubble.show_bubble(screen, plant.pos_x, plant.pos_y, plant.anger_level)
+
         for event in pygame.event.get():
             # check if player wants to quit the game
             if event.type == pygame.QUIT:
                 running = False
+
             # check if mouse is clicked
-            elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN:
                 # check if in collision with a good plant; i.e. player wants to drag a plant
                 mouseX, mouseY = event.pos
                 # if there is collision, keep dragging good plant
@@ -101,22 +182,42 @@ while running:
                         dragging = True
                         dragged_type = "Good"
                         dragged_ind = i
+                        GoodPlantList[i].clear_shelf()
                         GoodPlantList[i].drag_good_plant(mouseX, mouseY)
 
+                if vodka.check_vodka_collision(mouseX, mouseY):
+                    dragging = True
+                    dragged_type = "Vodka"
+                    print("hoho")
+
             # mouse up-- mouse click ends
-            elif event.type == pygame.MOUSEBUTTONUP:
+            if event.type == pygame.MOUSEBUTTONUP:
                 # if dragging plant before, stop dragging
                 if dragging:
                     dragging = False
-                    # check to see if player has dragged plant to a trash can, if so, delete plant obj
-                    for i in range(GoodPlant_length):
-                        if GoodPlantList[i].check_trash_good_collision(trash_can):
-                            # since the del function doesn't really do anything according to Google, let's just change the array length
-                            GoodPlant_length -= 1
-                            AllPlant_count -= 1
-                            # make sure we delete the right object by shifting all objects after forward 1 spot
-                            for j in range(i, GoodPlant_length):
-                                GoodPlantList[j] = GoodPlantList[j + 1]
+                    # TODO: added this if statement, don't know if its right
+                    if dragged_type == "Good":
+                        delete = False
+                        # check to see if player has dragged plant to a trash can, if so, delete plant obj
+                        # FIXME: can just use len(GoodPlantList)
+                        for i in range(GoodPlant_length):
+                            if GoodPlantList[i].check_trash_good_collision(trash_can):
+                                # since the del function doesn't really do anything according to Google,
+                                # let's just change the array length
+                                delete = True
+                                GoodPlant_length -= 1
+                                AllPlant_count -= 1
+                                # make sure we delete the right object by shifting all objects after forward 1 spot
+                                for j in range(i, GoodPlant_length):
+                                    GoodPlantList[j] = GoodPlantList[j + 1]
+                                break  # break out of for loop
+
+                        # end of for loop; if plant not deleted
+                        if ~delete:
+                            GoodPlantList[dragged_ind].correct_good_pos()
+
+                    elif dragged_type == "Vodka":
+                        vodka.check_plant_vodka_collision(GoodPlantList, vodka_count)
 
                 # if not dragging before, check if clicked on the next button
                 else:
@@ -129,18 +230,13 @@ while running:
                             stage = 0
 
             # if still dragging, make plant follow mouse pos
-            elif event.type == pygame.MOUSEMOTION:
+            if event.type == pygame.MOUSEMOTION:
                 if dragging:
                     mouseX, mouseY = event.pos
                     if dragged_type == "Good":
                         GoodPlantList[dragged_ind].drag_good_plant(mouseX, mouseY)
-
-        red = 0
-        green = 255
-        for i in range(len(shelf_rect_list)):
-            pygame.draw.rect(screen, (red, green, 0), shelf_rect_list[i])
-            red += 25
-            green -= 25
+                    elif dragged_type == "Vodka":  # TODO: here
+                        vodka.drag_vodka_bottle(mouseX, mouseY, vodka_count)
 
         # show trash can on screen
         trash_can.show_trash_can(screen)
@@ -149,11 +245,35 @@ while running:
         # Show all good plants' image
         for i in range(GoodPlant_length):
             GoodPlantList[i].show_good_plant(screen)
+        # show vodka bottle on screen
+        vodka.show_vodka_image(screen)
 
-    else: # for all other stages; place holder
+    else:  # for all other stages; place holder
         # Set background first; in loop b/c need constant display
         # RGB values, from 0 to 255
         screen.fill((255, 255, 255))
+
+        # show all messages from big grandpa
+        if big_grandpa_text_num < len(big_grandpa_text):
+            # show chat box
+            screen.blit(chat_box, chat_box_top_left)
+
+            # show big grandpa
+            big_grandpa.show_big_grandpa_font(screen),
+
+            # show message
+            show_text_in_box(screen, font, 250, 780, 170,
+                             big_grandpa_text_str[big_grandpa_text_num], (0, 0, 0))
+            #screen.blit(big_grandpa_text[big_grandpa_text_num], (0, 0))
+
+            for event in pygame.event.get():
+                # check if player wants to quit the game
+                if event.type == pygame.QUIT:
+                    running = False
+                # check if mouse is clicked to proceed to next message
+                if event.type == pygame.MOUSEBUTTONUP:
+                    if big_grandpa_text_num < len(big_grandpa_text):
+                        big_grandpa_text_num += 1
 
         for event in pygame.event.get():
             # check if player wants to quit the game
