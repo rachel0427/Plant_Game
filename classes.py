41a00674386e970fe0e854627788da4c69248229
Rachel <ruoqi.yang@vanderbilt.edu>
2021-06-26 15:23:34 +0800

    click arrow to proceed to next page which has nothing on it

--- classes.py
+++ classes.py
@@ -9,16 +9,23 @@ from pygame import mixer
 # round_num keeps track of the current round number
 global round_num
 global plant_size
+global shelf_rect_list
 
 
 # Some Variables
 InitialMoney = 50
 plant_size = 64
+# rectangle values: left, top, width, height
+shelf_rect_list = [pygame.Rect(270,178,120,100), pygame.Rect(390,178,120,100), pygame.Rect(510,178,120,100),
+                   pygame.Rect(270,315,120,100), pygame.Rect(390,315,120,100), pygame.Rect(510,315,120,100),
+                   pygame.Rect(270,453,120,100), pygame.Rect(390,453,120,100), pygame.Rect(510,453,120,100)]
+
 
 GoodPlantImage = 'Images/goodPlant.png'
 GreatPlantImage = 'Images/greatPlant.png'
 AwesomePlantImage = 'Images/awesomePlant.png'
-LeekImage = 'Images/Leek.png'
+LeekImage = 'Images/leek.png'
+TrashCanImage = 'Images/trashCan.png'
 
 ExplodeState = 3  # plants explode when anger state reaches 3
 
@@ -49,9 +56,23 @@ class Game:
 
 
 # Button Proceed
-class ButtonProceed:
-    pass
+class NextButton:
+    img = pygame.image.load('Images/next.png')
+    pos_x = 10
+    pos_y = 570
 
+    # show_next_button shows the next button on screen
+    def show_next_button(self, window):
+        window.blit(self.img, (self.pos_x, self.pos_y))
+
+    # check_next_collision checks for collision between mouse and next button
+    def check_next_collision(self, mouseX, mouseY):
+        # This is how you check for collision between mouse pos and objects
+        rectangle = self.img.get_rect()
+        rectangle.topleft = (self.pos_x, self.pos_y)
+        if rectangle.collidepoint(mouseX, mouseY):
+            return True
+        return False
 
 # Anger Bar
 class AngerBar:
@@ -103,6 +124,15 @@ class Bank:
     def awesome_produce(self):
         self.money += AwesomeProduce
 
+# Trash can object: drag plants to trash icon to delete object
+class TrashCan:
+    img = pygame.image.load(TrashCanImage)
+    pos_x = 820
+    pos_y = 570
+
+    # show_trash_can shows the trash can object on screen
+    def show_trash_can(self, window):
+        window.blit(self.img, (self.pos_x, self.pos_y))
 
 # Plants: 4 species
 # Class for Good Plants
@@ -113,20 +143,19 @@ class GoodPlant:
     production = GoodProduce
     production_cycle = GoodProduceCycle
     img = pygame.image.load(GoodPlantImage)
+    anger_state = 0
 
     # Constructor
-    def __init__(self, anger_state, pos_x, pos_y, shelf_pos):
-
-        self.anger_state = 0  # Initialized to 0; start out not angry
+    def __init__(self, pos_x, pos_y, shelf_pos):
         self.pos_x = pos_x
         self.pos_y = pos_y
-        self.shelf_pos = shelf_pos
+        self.shelf_pos = 0  # Initialized to top shelf
 
     # show_good_plant shows the good plant object on screen
     def show_good_plant(self, window):
         window.blit(self.img, (self.pos_x, self.pos_y))
 
-    # check_good_collision checks for collision with good plant
+    # check_good_collision checks for collision between mouse and good plant
     def check_good_collision(self, mouseX, mouseY):
         # This is how you check for collision between mouse pos and objects
         rectangle = self.img.get_rect()
@@ -139,6 +168,18 @@ class GoodPlant:
     def drag_good_plant(self, mouseX, mouseY):
         self.pos_x = mouseX - plant_size / 2
         self.pos_y = mouseY - plant_size / 2
+
+    # check_trash_plant_collision checks for collision with trash can
+    def check_trash_good_collision(self, trash_obj):
+        # This is how you check for collision between trash can and plant objects
+        distance = math.sqrt(
+            math.pow((trash_obj.pos_x - self.pos_x), 2) + math.pow((trash_obj.pos_y - self.pos_y), 2))
+        if distance < 57:
+            return True
+        return False
+    # correct_good_pos corrects good plant's position as player drags it on the shelf
+    def correct_good_pos(self):
+        pass
 
     def increase_anger(self):
         """Called when plant is moved up the shelf
--- main.py
+++ main.py
@@ -8,11 +8,17 @@ global screen_width
 global screen_height
 global shelf_size
 global dragging
+global GoodPlant_length  # length of the GoodPlant array
+global AllPlant_count
+global dragged_type
+global dragged_ind
+global stage  # stage keeps track of which page game is currently on
 
-screen_width = 800
-screen_height = 600
+screen_width = 900
+screen_height = 650
 shelf_size = 512
 dragging = False
+stage = 0  # in stage 0, player can vodka and drag plants on shelf
 
 # Initialize pygame
 pygame.init()
@@ -23,49 +29,135 @@ center_of_screen = (screen_width / 2, screen_height / 2)
 
 # Background
 background = pygame.image.load('Images/shelf1.png')
-background_top_left = (center_of_screen[0] - shelf_size / 2, center_of_screen[1] - shelf_size / 2)
+background_top_left = (center_of_screen[0] - shelf_size / 2, center_of_screen[1] - shelf_size / 2 + 40)
 
 # Title and Icon
-pygame.display.set_caption("Plants of Grudge")
+pygame.display.set_caption("Plants of Wrath")
 icon = pygame.image.load('Images/plantIcon.png')
 pygame.display.set_icon(icon)
 
 # Array for good_plant objects
-good1 = GoodPlant(0, 0, 0, 0)
-# GoodPlant = []
+GoodPlantList = []
+GoodPlant_length = len(GoodPlantList)
+
+# AllPlant_count keeps track of how many plants in total are present
+AllPlant_count = GoodPlant_length
+
+
+# function for creating new good plants
+def create_new_good_plant():
+    global GoodPlant_length
+    global AllPlant_count
+    # determine pos_x by checking if another plant is present already
+    # go through all plant instances on screen to see how many are on top of screen
+    inLine = 0
+    for i in range(GoodPlant_length):
+        if GoodPlantList[i].pos_y < 113:
+            inLine += 1
+    # Suppose we only have less than 11 plants in line
+    pos_x = 10 + inLine * 70
+    pos_y = 10
+    shelf_pos = 0
+
+    good_tmp = GoodPlant(pos_x, pos_y, shelf_pos)
+    GoodPlantList.append(good_tmp)
+    GoodPlant_length += 1
+    AllPlant_count += 1
+
+
+# Create trash can object
+trash_can = TrashCan()
+
+# Create next button object
+next_button = NextButton()
+
+# Create 3 good plants
+for i in range(3):
+    create_new_good_plant()
 
 # running
 running = True
+
 while running:
-    # Set background first; in loop b/c need constant display
-    # RGB values, from 0 to 255
-    screen.fill((255, 255, 255))
+    if stage == 0:
+        # Set background first; in loop b/c need constant display
+        # RGB values, from 0 to 255
+        screen.fill((255, 255, 255))
 
-    # Background Image
-    screen.blit(background, background_top_left)
+        # Background Image
+        screen.blit(background, background_top_left)
 
-    for event in pygame.event.get():
-        # check if player wants to quit the game
-        if event.type == pygame.QUIT:
-            running = False
-        # check if player wants to replace a plant
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            # check if in collision with a good plant
-            mouseX, mouseY = event.pos
-            # if there is collision, keep dragging good plant
-            if good1.check_good_collision(mouseX, mouseY):
-                dragging = True
-                good1.drag_good_plant(mouseX, mouseY)
-
-        # mouse up-- stop dragging
-        elif event.type == pygame.MOUSEBUTTONUP:
-            dragging = False
-
-        # if still dragging, make plant follow mouse pos
-        elif event.type == pygame.MOUSEMOTION:
-            if dragging:
+        for event in pygame.event.get():
+            # check if player wants to quit the game
+            if event.type == pygame.QUIT:
+                running = False
+            # check if mouse is clicked
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                # check if in collision with a good plant; i.e. player wants to drag a plant
                 mouseX, mouseY = event.pos
-                good1.drag_good_plant(mouseX, mouseY)
+                # if there is collision, keep dragging good plant
+                for i in range(GoodPlant_length):
+                    if GoodPlantList[i].check_good_collision(mouseX, mouseY):
+                        dragging = True
+                        dragged_type = "Good"
+                        dragged_ind = i
+                        GoodPlantList[i].drag_good_plant(mouseX, mouseY)
 
-    good1.show_good_plant(screen)
+            # mouse up-- mouse click ends
+            elif event.type == pygame.MOUSEBUTTONUP:
+                # if dragging plant before, stop dragging
+                if dragging:
+                    dragging = False
+                    # check to see if player has dragged plant to a trash can, if so, delete plant obj
+                    for i in range(GoodPlant_length):
+                        if GoodPlantList[i].check_trash_good_collision(trash_can):
+                            # since the del function doesn't really do anything according to Google, let's just change the array length
+                            GoodPlant_length -= 1
+                            AllPlant_count -= 1
+                            # make sure we delete the right object by shifting all objects after forward 1 spot
+                            for j in range(i, GoodPlant_length):
+                                GoodPlantList[j] = GoodPlantList[j + 1]
+
+                # if not dragging before, check if clicked on the next button
+                else:
+                    mouseX, mouseY = event.pos
+                    if next_button.check_next_collision(mouseX, mouseY):
+                        # if stage != 2
+                        if stage < 2:
+                            stage += 1
+                        else:
+                            stage = 0
+
+            # if still dragging, make plant follow mouse pos
+            elif event.type == pygame.MOUSEMOTION:
+                if dragging:
+                    mouseX, mouseY = event.pos
+                    if dragged_type == "Good":
+                        GoodPlantList[dragged_ind].drag_good_plant(mouseX, mouseY)
+
+        red = 0
+        green = 255
+        for i in range(len(shelf_rect_list)):
+            pygame.draw.rect(screen, (red, green, 0), shelf_rect_list[i])
+            red += 25
+            green -= 25
+
+        # show trash can on screen
+        trash_can.show_trash_can(screen)
+        # show next button on screen
+        next_button.show_next_button(screen)
+        # Show all good plants' image
+        for i in range(GoodPlant_length):
+            GoodPlantList[i].show_good_plant(screen)
+
+    else: # for all other stages; place holder
+        # Set background first; in loop b/c need constant display
+        # RGB values, from 0 to 255
+        screen.fill((255, 255, 255))
+
+        for event in pygame.event.get():
+            # check if player wants to quit the game
+            if event.type == pygame.QUIT:
+                running = False
+
     pygame.display.update()
